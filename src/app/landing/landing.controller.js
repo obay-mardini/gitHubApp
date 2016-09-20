8931988c2e910dc55956e33cfd3855dbc2ce1994
obay2 <obaymardini@gmail.com>
2016-09-20 02:48:59 +0200

    get the data from github and list the repos links

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -8,5 +8,21 @@
     function LandingController() {
         var vm = this;
         vm.name = 'obay'
+        vm.searchGitHub = searchGitHub;
+        vm.addRepo = addRepo;
+        
+        function searchGitHub() {
+            $.get('https://api.github.com/users/goeuro/repos', function(repos) {
+                vm.repos = repos.map(function(repo){
+                   return {url: repo.html_url, name: repo.name} 
+                });
+            });        
+        }
+        
+        function addRepo(data) {
+            data.forEach(function(repo){
+                $repos.append(repo.name)
+            });
+        }
     }
 })();
